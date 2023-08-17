20a0d0ab2052febc870d8040d371cc0425c85cfe
Ilya Irmashev <ilya290397@gmail.com>
2023-08-18 00:42:36 +0400

    update str_func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,12 @@
 def capital_letters():
+    """Функция возвращает строку заглавными буквами"""
     print("i am learning python".upper())
 
 capital_letters()
+
+
+def another_capital_letters():
+    """Функция делает заглавными первые буквы каждого слова в строке"""
+    print(input('Ввод строки: ').title())
+
+another_capital_letters()
