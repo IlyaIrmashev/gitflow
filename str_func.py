a51fc8e58579220756b62ffda2eb910a97a3cd33
IlyaIrmashev <142181172+IlyaIrmashev@users.noreply.github.com>
2023-08-18 00:46:51 +0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,10 +3,3 @@ def capital_letters():
     print("i am learning python".upper())
 
 capital_letters()
-
-
-def another_capital_letters():
-    """Функция делает заглавными первые буквы каждого слова в строке"""
-    print(input('Ввод строки: ').title())
-
-another_capital_letters()
